ae87bc3f4ba3aa4299d981b803e97b6dc8498ebe
AlexandreNoguez <alexandrenoguez_rs@yahoo.com.br>
2025-08-27 21:50:33 -0300

    docs(swagger): add OpenAPI bean (server url and ErrorResponse schema)

---
+++ src/main/java/com/dio/bank/bank_ddd/interfaces/config/OpenApiConfig.java
@@ -0,0 +1,35 @@
+package com.dio.bank.bank_ddd.interfaces.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.servers.Server;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.media.Schema;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI api() {
+        return new OpenAPI()
+            .servers(List.of(new Server().url("http://localhost:8080")))
+            .components(new Components()
+                .addSchemas("ErrorResponse",
+                    new Schema<>()
+                        .addProperties("statusCode", new Schema<>().type("integer").format("int32"))
+                        .addProperties("error", new Schema<>().type("string"))
+                        .addProperties("message", new Schema<>().type("string"))
+                )
+            )
+            .info(new io.swagger.v3.oas.models.info.Info()
+                .title("DIO Bank DDD API")
+                .version("0.0.1")
+                .description("Bank backend with Accounts, PIX and Investments (DDD).")
+                .license(new License().name("MIT"))
+            );
+    }
+}
